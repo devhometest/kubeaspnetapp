de16807ae10dc09c7f6b9fd531c42951e436f168
vansonvinhuni <vansonvinhuni@gmail.com>
2019-11-12 17:04:39 +0700

    Init

---
+++ kubeaspnetapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kubeaspnetapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
